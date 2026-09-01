92b5cabe4d94fa9831f54dc8a6437f7ae762341a
saranphotog123-collab <saran.photog123@gmail.com>
2026-09-01 21:58:31 +0530

    Added API Design

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+
+class Student(BaseModel):
+    name: str
+    age: int
+    department: str
+    email: str
+
+@app.post("/students")
+def create_student(student: Student):
+    return student
